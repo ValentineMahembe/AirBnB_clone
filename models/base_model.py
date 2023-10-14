3451f28bd08ea1c5c42942df2d4e413f14309d40
Valentine Mahembe <mahembevalentine@gmail.com>
2023-10-14 13:35:45 -0700

    Second commit - 4. Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -5,24 +5,25 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
+
     """This class represents a base model for other classes"""
 
     def __init__(self, *args, **kwargs):
-        """Initializes a new instance of BaseModel"""
-        if kwargs:
-            # if kwargs is not empty, create an instance from a dictionary
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    # convert string to datetime object
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    # set the attribute with the value
-                    setattr(self, key, value)
-        else:
-            # if kwargs is empty, create a new instance with default values
-            self.id = str(uuid.uuid4()) # generate a unique id
-            self.created_at = datetime.now() # assign current datetime
-            self.updated_at = self.created_at # assign current datetime
+    """Initializes a new instance of BaseModel"""
+    if kwargs:
+        # if kwargs is not empty, create an instance from a dictionary
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                # convert string to datetime object
+                value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+            if key != "__class__":
+                # set the attribute with the value
+                setattr(self, key, value)
+    else:
+        # if kwargs is empty, create a new instance with default values
+        self.id = str(uuid.uuid4()) # generate a unique id
+        self.created_at = datetime.now() # assign current datetime
+        self.updated_at = self.created_at # assign current datetime
 
     def __str__(self):
         """Returns a string representation of the instance"""
